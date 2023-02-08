68ddde49ccbb8b513b041e4b673ff9a97763d716
brunoribeiro122 <93730878+brunoribeiro122@users.noreply.github.com>
2023-02-08 23:10:19 +0000

    Primeiro  Código git

---
+++ ex1.py
@@ -0,0 +1,14 @@
+n1 = float(input("Entre com n1: "))
+n2 = float(input("Entre com n2: "))
+n3 = float(input("Entre com n3: "))
+me = float(input("Entre com me: "))
+
+ma = (n1 + n2*2 +n3*3 + me)/7
+
+if (ma >= 9):
+    print ("Maior ou Igual a 9")
+
+
+if (ma < 4):
+    print ("Menor que 4")
+    
